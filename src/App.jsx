c1527b09771b88d34569a497e7509954b0f4c35a
Makorii <marcelaromina.d@hotmail.com>
2024-02-06 17:40:31 -0300

    armo enrutado para las opciones en el navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import Navbar from "./components/Navbar"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Knowledge from "./components/Knowledge"
+import Projects from "./components/Projects"
+import Contact from "./components/Contact"
+import Home from "./components/Home";
 
 function App() {
 
 
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/knowledge" element={<Knowledge />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
